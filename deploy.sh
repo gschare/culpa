db00b26dd0ca57f3e6032281e161a87afca774db
Gregory Schare <grschare1315@gmail.com>
2022-08-25 23:02:49 -0400

    tweak deploy

--- deploy.sh
+++ deploy.sh
@@ -8,4 +8,4 @@ stack build.hs
 git add -f $SITE_DIR && git commit -m "Build site $(date +'%d %b %Y %T')"
 git push -d origin $SITE_BRANCH     # delete branch
 git subtree push --prefix $SITE_DIR origin $SITE_BRANCH
-git reset HEAD~1                    # reset the commit only in the current branch
+#git reset HEAD~1                    # reset the commit only in the current branch
